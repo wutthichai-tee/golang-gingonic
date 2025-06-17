84623f76c9a0e104fdf9f4fe693b2b5f88015c3d
610597 <if610597@IF8000610597N1.local>
2025-06-18 00:00:47 +0700

    Update comment

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// http://localhost:8080/ping
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()
 }
